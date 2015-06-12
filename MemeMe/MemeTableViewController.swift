701acfe2ed2beb28eb98428c2a9d8383917f95af
Marcel Salathé <salathe.marcel@gmail.com>
2015-06-12 12:46:45 -0400

    hooked up table view with meme data

--- MemeMe/MemeTableViewController.swift
+++ MemeMe/MemeTableViewController.swift
@@ -9,10 +9,12 @@
 import Foundation
 import UIKit
 
-class MemeTableViewController: UITableViewController {
+class MemeTableViewController: UITableViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var memes: [Meme]!
+    
     
     override func viewDidLoad() {
-        println("viewDidLoad")
         super.viewDidLoad()
         self.title = ""
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target: self, action: "presentMemeEditor")
@@ -20,13 +22,27 @@ class MemeTableViewController: UITableViewController {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
+        let object = UIApplication.sharedApplication().delegate
+        let appDelegate = object as! AppDelegate
+        memes = appDelegate.memes
+        self.tableView.reloadData()
     }
     
     func presentMemeEditor() {
         var memeEditorController:ViewController
         memeEditorController = self.storyboard?.instantiateViewControllerWithIdentifier("MemeEditorViewController") as! ViewController
         self.presentViewController(memeEditorController, animated: true, completion: nil)
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MemeCell") as! UITableViewCell
+        cell.imageView?.image = memes[indexPath.row].memedImage
+        cell.textLabel?.text = "bla"
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.memes.count
     }
     
 }
